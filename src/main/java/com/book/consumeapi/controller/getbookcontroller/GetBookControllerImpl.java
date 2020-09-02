fdd021619a199c5a0fbb54478696531ce8659279
avrm <avrmeduard@gmail.com>
2020-09-02 22:15:07 +0300

    logger added

--- src/main/java/com/book/consumeapi/controller/getbookcontroller/GetBookControllerImpl.java
+++ src/main/java/com/book/consumeapi/controller/getbookcontroller/GetBookControllerImpl.java
@@ -5,6 +5,8 @@ import com.book.consumeapi.model.getbook.GetBook;
 import com.book.consumeapi.model.getbook.GetBookResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -17,6 +19,8 @@ import java.util.Optional;
 
 @RestController
 public class GetBookControllerImpl implements GetBookController{
+
+    private Logger log = LoggerFactory.getLogger(GetBookControllerImpl.class);
 
     @Value("${mvc.basepath}")
     private String basePath;
@@ -33,7 +37,13 @@ public class GetBookControllerImpl implements GetBookController{
     @Override
     public ResponseEntity<?> getBookSummary(Optional<Integer> bookId) {
 
+        log.info("Called /rest-getBook");
+        log.debug("Called /rest-getBook witch bookId = " + bookId);
+
         if (bookId.isPresent()) {
+
+            log.info("BookId is present");
+            log.debug("BookId = " + bookId.get());
 
             HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.add(httpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
@@ -42,10 +52,10 @@ public class GetBookControllerImpl implements GetBookController{
             HttpEntity httpEntity = new HttpEntity(httpHeaders);
 
             ResponseEntity<String> responseEntity = restTemplate.exchange(basePath+getEndPoint+"?bookId={id}",
-                                                               HttpMethod.GET,
-                                                               httpEntity,
-                                                               String.class,
-                                                               bookId.get() );
+                    HttpMethod.GET,
+                    httpEntity,
+                    String.class,
+                    bookId.get() );
 
             System.out.println(responseEntity.getBody());
 
@@ -55,13 +65,17 @@ public class GetBookControllerImpl implements GetBookController{
                 try {
 
                     GetBookResponse getBook = objectMapper.readValue(responseEntity.getBody(), GetBookResponse.class);
-                    System.out.println(getBook.toString());
 
+                    log.info("Book found and added to the list");
+                    log.debug("Book found and added: " + getBook.toString());
 
                     //
                     return new ResponseEntity<>(getBook, HttpStatus.OK);
                 } catch (JsonProcessingException e) {
                     e.printStackTrace();
+
+                    log.info("No book was found");
+                    log.debug("No book was found whit bookId = " + bookId.get());
 
                     return new ResponseEntity<>("Invalid request", HttpStatus.BAD_REQUEST);
                 }
