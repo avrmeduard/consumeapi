688f93a11f9d5b520004a02d97faa0d9ee34d05b
avrm <avrmeduard@gmail.com>
2020-09-02 21:15:43 +0300

    error response model added

--- src/main/java/com/book/consumeapi/model/errormodel/ErrorResponse.java
+++ src/main/java/com/book/consumeapi/model/errormodel/ErrorResponse.java
@@ -1,4 +1,19 @@
 package com.book.consumeapi.model.errormodel;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
+
+    @JsonProperty("errorDescription")
+    private String errorDescription;
+
+    public String getErrorDescription() {
+        return errorDescription;
+    }
+
+    public void setErrorDescription(String errorDescription) {
+        this.errorDescription = errorDescription;
+    }
 }
